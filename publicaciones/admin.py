6844f7b7ade76db93cdfb014b96fdd45bb61887e
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:53:11 -0600

    Update admin.py

--- publicaciones/admin.py
+++ publicaciones/admin.py
@@ -1,6 +1,8 @@
+# by: RETBOT
 from django.contrib import admin
 from .models import Publicacion, Comentario
 
 # Register your models here.
 admin.site.register(Publicacion)
 admin.site.register(Comentario)
+# by: RETBOT
