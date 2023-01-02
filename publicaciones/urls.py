eb5a65205f1a759624b09d1e8d7a9834b0246ce7
RETBOT <robertoesquiveltr16@gmail.com>
2023-01-01 18:14:14 -0600

    modif

--- publicaciones/urls.py
+++ publicaciones/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('',VistaListaPublicaciones.as_view(), name='lista_publicaciones'),
     # Publicaciones 
     path('nuevo/',VistaCrearPublicacion.as_view(), name='nueva_publicacion'),
-    path('<int:pk>/editar/',VistaEditarPublicacion.as_view(), name='editar_publicacion'),
+    path('<uuid:pk>/editar/',VistaEditarPublicacion.as_view(), name='editar_publicacion'),
     path('<int:pk>/detalle/',VistaDetallePublicacion.as_view(), name='detalle_publicacion'),
     path('<int:pk>/eliminar/',VistaEliminarPublicacion.as_view(), name='eliminar_publicacion'),
 
