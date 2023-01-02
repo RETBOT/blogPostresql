80fb46c146e843888423698017cdcfff13413388
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:43:00 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+# by: RETBOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -28,3 +28,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+# by: RETBOT
