a16e6122c0750961f917ed85ba755420ece27794
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:52:57 -0600

    Update models.py

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,8 +1,9 @@
 #publicaciones/models.py
+# by: RETBOT
 from django.db import models 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
+# by: RETBOT
 class Publicacion(models.Model):
   titulo = models.CharField(max_length=200)
   autor = models.ForeignKey(
@@ -17,7 +18,7 @@ class Publicacion(models.Model):
   
   def get_absolute_url(self):
     return reverse('detalle_publicacion',args=[str(self.id)])
-  
+  # by: RETBOT
 class Comentario(models.Model):
   titulo = models.ForeignKey(
       Publicacion, 
@@ -34,6 +35,7 @@ class Comentario(models.Model):
 
   def __str__(self):
     return self.comentario
-    
+    # by: RETBOT
   def get_absolute_url(self):
     return reverse("lista_publicaciones")
+  # by: RETBOT
