ccbea45121a7db340e10b6586a9e9163cc807a2d
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:44:07 -0600

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 # blog/urls.py 
 from django.urls import path
 from .views import VistaPaginaInicio 
-   
+   # by: RETBOT
 urlpatterns = [ 
   path('', VistaPaginaInicio.as_view(), name='inicio'),
-] 
+] # by: RETBOT
