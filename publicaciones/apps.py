9c852b18b54854532f56801ed0fb8c7cd3dcc3c4
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:53:03 -0600

    Update apps.py

--- publicaciones/apps.py
+++ publicaciones/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+# by: RETBOT
 
 class PublicacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'publicaciones'
+    
+    
+    # by: RETBOT
